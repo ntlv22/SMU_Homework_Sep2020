a1ffdf4be0e2a9f81db36fbef4d54f4ddb169e67
ntlv22 <71297803+ntlv22@users.noreply.github.com>
2020-12-09 00:54:24 -0600

    hw 10

--- 06-Python-APIs/Submission/starter_code/api_keys.py
+++ 06-Python-APIs/Submission/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "c04681263ed662bbb1c856237ce3a9be"
+weather_api_key = "Put Your Key Here"
 
 # Google API Key
-g_key = "AIzaSyCSlOhD6LeCQuCIe51z-v0C9jGE0Ztgc9g"
+g_key = "PUT YOUR KEY HERE!"
